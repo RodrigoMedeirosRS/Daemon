dbc1eb2fe01125ff045c28047d4bc195a946389d
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2020-11-20 14:40:42 -0300

    feat(mecanicas)
    Implementadas rolagens de dados e dano das armas.

---
+++ System.Daemon/Rolagem.cs
@@ -0,0 +1,17 @@
+using RandomGen;
+using System.Daemon.Interface;
+
+namespace System.Daemon
+{
+    public class Rolagem : IRolagem
+    {
+        public float RolarD100()
+        {
+            return Convert.ToSingle(Gen.Random.Numbers.Doubles(0.0, 1.0));
+        }
+        public int RolarDado(int quantidade, int faces, int modificador)
+        {
+            return Gen.Random.Numbers.Integers(quantidade, quantidade * faces)() + modificador;
+        }
+    }
+}
